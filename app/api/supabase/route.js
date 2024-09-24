b51dc5bc2002299c24b3adc8541e83fc8ef77313
Shaun Zhou <shaunzhoujx@yahoo.com>
2024-09-24 16:49:48 +0800

    Added `schedule` folder in `api`

--- app/api/supabase/route.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-// Initialize Supabase client
-const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_KEY
-);
-
-export default supabase;
--- app/apply/page.js
+++ app/apply/page.js
@@ -7,11 +7,36 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list"; // Import listPlugin for list views
 import styles from "../styles/applications.module.css"; // Import the CSS module
+import { createClient } from "@supabase/supabase-js";
 
-import {
-    fetchApprovedArrangements,
-    formatDate,
-} from "../api/applications/route.js"; // Import service functions
+// Initialize Supabase client
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_KEY
+);
+
+const fetchApprovedArrangements = async () => {
+    const { data, error } = await supabase
+        .from("arrangement")
+        .select(`*,employee:staff_id (staff_fname, staff_lname)`)
+        .eq("status", "approved")
+        .order("date", { ascending: true });
+
+    return { data, error };
+};
+
+/**
+ * Formats a JavaScript Date object into 'YYYY-MM-DD' string.
+ *
+ * @param {Date} date - The date to format.
+ * @returns {string} Formatted date string.
+ */
+const formatDate = (date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+};
 
 const ApprovedArrangements = () => {
     // State Variables
