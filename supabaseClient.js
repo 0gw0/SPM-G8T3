f33e8f42b4b8f77d01b5be8dc1e4ccc3bc999cdb
Shaun Zhou <shaunzhoujx@yahoo.com>
2024-10-01 14:59:59 +0800

    Deleted supabaseClient.js
    Deleted separate supabase client file which shouldn't exist.

--- supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-// Supabase URL and Key from environment variables
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
-
-// Create and export the Supabase client
-export const supabase = createClient(supabaseUrl, supabaseKey);
